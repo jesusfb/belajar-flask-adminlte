64bd9bec98a05faedb5b0623b44a9dd4689887d7
idoz <firdaus.gymnastiar@gmail.com>
2024-04-29 01:30:14 +0700

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,88 @@
+function generateRandomNumber() {
+  // Membuat angka acak antara 100000 dan 999999 (6 digit)
+  var randomNumber = Math.floor(100000 + Math.random() * 900000);
+  // Menampilkan angka acak di console
+  console.log("Angka acak: " + randomNumber);
+  // Menampilkan angka acak di halaman HTML
+  document.getElementById("randomNumberDisplay").value = randomNumber;
+}
+
+// Add event listener to the form
+// document
+//   .getElementById("usernameForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent the form from submitting normally
+
+//     // Get the username value from the form
+//     var username = document.getElementById("nim").value;
+
+//     // Simulate form submission with AJAX
+//     fetch("/tes", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ username: username }),
+//     })
+//       .then((response) => {
+//         if (response.ok) {
+//           // If the response is successful, show success message
+//           Swal.fire({
+//             icon: "success",
+//             title: "Success!",
+//             text: "Username submitted successfully: " + username,
+//           });
+//         } else {
+//           // If there is an error, show error message
+//           Swal.fire({
+//             icon: "error",
+//             title: "Error!",
+//             text: "Failed to submit username. gagal di database.",
+//           });
+//         }
+//       })
+//       .catch((error) => {
+//         // If there is an error, show error message
+//         Swal.fire({
+//           icon: "error",
+//           title: "Error!",
+//           text: "Failed to submit username. gagal di promise.",
+//         });
+//       });
+//   });
+
+// function login() {
+//   var username = document.getElementById("nim").value;
+//   var password = document.getElementById("programStudi").value;
+
+//   // Kirim data login ke server menggunakan AJAX
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", "/tes", true);
+//   xhr.setRequestHeader("Content-Type", "application/json");
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//       if (xhr.status === 200) {
+//         var response = JSON.parse(xhr.responseText);
+//         if (response.success) {
+//           // Jika login berhasil, tampilkan popup SweetAlert
+//           Swal.fire({
+//             icon: "success",
+//             title: "Login Successful!",
+//             text: "Welcome " + username,
+//           });
+//         } else {
+//           // Jika login gagal, tampilkan popup SweetAlert
+//           Swal.fire({
+//             icon: "error",
+//             title: "Login Failed!",
+//             text: response.message,
+//           });
+//         }
+//       } else {
+//         console.error(xhr.responseText);
+//       }
+//     }
+//   };
+//   var data = JSON.stringify({ username: username, password: password });
+//   xhr.send(data);
+// }
---
+++ web.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskadminlte_db'
+mysql = MySQL(app)
+
+
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html',menu='register')
+
+@app.route("/registerwajah", methods=["POST"])
+def registerwajah():
+    email = request.form['email']
+    nama = request.form['nama']
+    nim = request.form['nim']
+    prodi = request.form['prodi']
+    angkatan = request.form['angkatan']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO mahasiswaterdaftar(email,nama,nim,prodi,angkatan) VALUES(%s,%s,%s,%s,%s)", (email,nama,nim,prodi,angkatan))
+    mysql.connection.commit()
+    cur.close()
+    return render_template('register.html',menu='register')
+
+@app.route("/login")
+def login():
+    return render_template('login.html',menu='login')
+
+# @app.route("/tes", methods=["POST"])
+# def ngetes():
+#     # if request.method == 'POST':
+#     #     # Mengambil data username dari permintaan POST
+#     #     username = request.json.get('nim')
+        
+#     #     # Periksa apakah username sudah ada di database
+#     #     cursor = mysql.connection.cursor()
+#     #     cursor.execute("SELECT * FROM mahasiswaterdaftar WHERE nim = %s", (username,))
+#     #     existing_user = cursor.fetchone()
+
+#     #     if existing_user:
+#     #         # Jika username sudah ada, kirim respons dengan pesan error
+#     #         response = {'status': 'error', 'message': 'Username already exists in the database'}
+#     #         cursor.close()
+#     #         return jsonify(response), 400
+
+#     #     # Jika username belum ada, simpan data ke database
+#     #     cursor.execute("INSERT INTO mahasiswaterdaftar (nim) VALUES (%s)", (username,))
+#     #     mysql.connection.commit()
+#     #     cursor.close()
+
+#     #     # Buat respons sukses
+#     #     response = {'status': 'success', 'message': 'Username submitted successfully'}
+
+#     #     # Mengirim respons JSON kembali ke klien
+#     #     return jsonify(response), 200
+#     # else:
+#     #     # Jika metode tidak diizinkan, kirim respons JSON dengan status 405 (Method Not Allowed)
+#     #     response = {'status': 'error', 'message': 'Method not allowed'}
+#     #     return jsonify(response), 405
+
+#     nim = request.form['nim']
+#     prodi = request.form['prodi']
+
+#     # Mengambil data pengguna dari database
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT * FROM mahasiswaterdaftar WHERE nim = %s", (nim,)) 
+#     user = cur.fetchone()
+#     cur.close()
+#     # if user:
+#     #     # Konversi tupel menjadi dictionary
+#     #     user_dict = {
+#     #     'nim': user[0],
+#     #     'prodi': user[1]
+#     #     }
+
+#     #     if user and user_dict['prodi'] == prodi:
+#     #         # Jika login berhasil, kirim respon JSON
+#     #         return jsonify({"success": True})
+#     #     else:
+#     #         # Jika login gagal, kirim respon JSON dengan pesan kesalahan
+#     #         return jsonify({"success": False, "message": "Invalid username or password"})
+#     if user and user[1] == prodi:
+#     # Jika login berhasil, kirim respon JSON
+#         return jsonify({"success": True})
+#     else:
+#     # Jika login gagal, kirim respon JSON dengan pesan kesalahan yang lebih spesifik
+#         return jsonify({"success": False, "message": "Invalid username or prodi"})
+
+@app.route("/table")
+def table():
+    return render_template('table.html',menu='table')
+@app.route("/generate")
+def generate():
+    return render_template('generate.html',menu='generate')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
